2479867c6679a65eac35fb706b8b2fcf201fe336
ChoeSul <jjgod0124@gmail.com>
2018-08-10 23:57:23 +0900

    init 프로젝트 세팅 디펜던시추가

---
+++ app/src/main/java/com/mizing/todoapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mizing.todoapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
